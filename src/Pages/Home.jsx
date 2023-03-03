accd574f226f97578a8a434579afe1781a00f306
Begimai <begimai.modueva.ch22@gmail.com>
2023-03-03 16:34:29 +0600

    functionality for sorting the Sort ASC, DESC component and filtiring pizzas

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,22 +7,36 @@ import Categories from '../components/categories/Categories';
 function Home() {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  const [categoryId, setCategoryId] = React.useState(0);
+  const [sortType, setSortType] = React.useState({
+    name: 'популярности',
+    sortProperty: 'rating',
+  });
 
-  /* ------ Fetch Запрос получаем от сервера данные------- */
+  /* ------ Fetch Запрос получаем от сервера данные ------- */
   React.useEffect(() => {
-    fetch('https://63f6d0179daf59d1ad8e8415.mockapi.io/items')
+    setIsLoading(true);
+
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const category = categoryId > 0 ? `category=${categoryId}` : '';
+
+    fetch(
+      `https://63f6d0179daf59d1ad8e8415.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
         setIsLoading(false);
       });
-      window.scrollTo(0, 0); // scroll верх в первом renderer
-  }, []);
+    window.scrollTo(0, 0); // scroll верх в первом renderer
+  }, [categoryId, sortType]);
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
-        <Sort />
+        <Categories value={categoryId} onClickCategory={(id) => setCategoryId(id)} />
+        <Sort value={sortType} onClickSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
