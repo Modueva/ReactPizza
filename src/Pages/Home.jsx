937bb43b16eabf2ce3cab76ffb27e4c21e1c56b8
Modueva <begimai.modueva.ch22@gmail.com>
2023-09-17 23:34:36 +0600

    Optimize the search using Debounce, pagination via Redux Toolkit

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { setCategoryId } from '../Redux/slices/filterSlice';
+import { setCategoryId , setCurrentPage} from '../Redux/slices/filterSlice';
 import PizzaBlock from '../components/pizzaBlock/PizzaBlock';
 import Skeleton from '../components/pizzaBlock/Skeleton';
 import Sort from '../components/sort/Sort';
@@ -10,18 +11,21 @@ import { SearchContext } from '../App';
 
 function Home() {
   const dispatch = useDispatch();
-  const {categoryId, sort} = useSelector((state) => state.filterSlice);
+  const { categoryId, sort, currentPage } = useSelector((state) => state.filterSlice);
 
   const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [currentPage, setcurrentPage] = React.useState(1);
 
   const onClickCategory = (id) => {
     dispatch(setCategoryId(id));
   };
 
-  /* ------ Fetch Запрос получаем от сервера данные ------- */
+  const onChangePage = number => {
+    dispatch(setCurrentPage(number))
+  }
+
+  /* ------ axios get Запрос получаем от сервера данные ------- */
   React.useEffect(() => {
     setIsLoading(true);
 
@@ -30,25 +34,19 @@ function Home() {
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
 
-    fetch(
-      `https://63f6d0179daf59d1ad8e8415.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
-    )
-      .then((res) => res.json())
-      .then((json) => {
-        setItems(json);
+    axios
+      .get(
+        `https://63f6d0179daf59d1ad8e8415.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
+      )
+      .then((res) => {
+        setItems(res.data);
         setIsLoading(false);
       });
+
     window.scrollTo(0, 0); // scroll верх в первом renderer
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
 
-  const pizzas = items
-    // .filter((obj) => {
-    //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-    //     return true;
-    //   }
-    //   return false;
-    // })
-    .map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
 
   const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
 
@@ -60,7 +58,7 @@ function Home() {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
-      <Pagination onChangePage={(number) => setcurrentPage(number)} />
+      <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
 }
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import debounce from 'lodash.debounce'
 import { SearchContext } from '../../App';
+
 import styles from './Search.module.scss';
 
+
+
 const Search = () => {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState('')
+  const {  setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    setValue('');
+    inputRef.current.focus();
+  }
+
+  const updateSearchValue =  React.useCallback(
+    debounce((str) =>{
+      setSearchValue(str);
+    }, 250),
+    []
+  );
+
+  const onChangeInput = event => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  }
+
   return (
     <div className={styles.root}>
       <svg
@@ -40,15 +65,16 @@ const Search = () => {
         />
       </svg>
       <input
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Поиск пиццы..."
         type="text"
       />
-      {searchValue && (
+      {value && (
         <svg
-          onClick={() => setSearchValue('')}
+          onClick={onClickClear}
           className={styles.closeIcon}
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg">
